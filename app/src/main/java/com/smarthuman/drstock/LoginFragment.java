eed369c70f58fa56ff0b92706ed251bb90f808e2
syz112679 <syz112679@gmail.com>
2018-03-06 23:01:04 +0800

    first commit from android studio

--- app/src/main/java/com/smarthuman/drstock/LoginFragment.java
+++ app/src/main/java/com/smarthuman/drstock/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.smarthuman.drstock;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -18,46 +19,32 @@ import java.util.ArrayList;
 
 public class LoginFragment extends android.support.v4.app.Fragment implements  View.OnClickListener{
 
-    String mUid;
-    private TextView mUserName;
-    private TextView mMoney;
-    private ArrayList<String> mMyStock;
-    private ArrayList<String> mFavorites;
+
     private Button mSignInbtn;
-    private Button mSignOutbtn;
     private Button mRegisterbtn;
-    private Button mMyStockbtn;
-    private Button mFavoritebtn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, null);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+
+        return view;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.sign_out:case R.id.user_sign_out:
-                ((MainActivity)getActivity()).mAuth.signOut();
 
-                Toast.makeText(getActivity(), "Signed Out...", Toast.LENGTH_SHORT).show();
 
-                mUserName.setText("Please sign in first");
-                mMoney.setText("0.0");
-                break;
-
-            case R.id.display_favorite:
-                Toast.makeText(getActivity(), "To My Favorites...", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.display_my_stock:
-                Toast.makeText(getActivity(), "To My Stocks...", Toast.LENGTH_SHORT).show();
+            case R.id.no_user_register:
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
                 break;
 
             case R.id.no_user_sign_in:
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
+                Intent intent2 = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent2);
                 break;
 
 
